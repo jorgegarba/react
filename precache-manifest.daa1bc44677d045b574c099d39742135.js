8d3e857979a51297e444d51c2c375535c2e88d3c
Jorge Garnica Blanco <jorgegarba@gmail.com>
2020-09-05 10:48:16 -0500

    d

---
+++ precache-manifest.daa1bc44677d045b574c099d39742135.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b08e71fd52ece2bda53fd72c54e7d2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b76f15c267b32d070a5e",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "d6dd2c34b77a22a34124",
+    "url": "/static/css/main.f0a0bb11.chunk.css"
+  },
+  {
+    "revision": "b76f15c267b32d070a5e",
+    "url": "/static/js/2.229aff16.chunk.js"
+  },
+  {
+    "revision": "94f4c95025d6d502a15431abd5dbc7cb",
+    "url": "/static/js/2.229aff16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6dd2c34b77a22a34124",
+    "url": "/static/js/main.3c0da4f8.chunk.js"
+  },
+  {
+    "revision": "7bf3b80bd52cdefbfcf3",
+    "url": "/static/js/runtime-main.2c9c0710.js"
+  }
+]);
